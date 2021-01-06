d0842a7f301f7668a5358bb837a3dbebc5182db9
May Thazin <maythazin@management-partners.co.jp>
2021-01-06 16:14:41 +0630

    Setting
    Setting

--- lib/views/settingScreen.dart
+++ lib/views/settingScreen.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:tis/views/term.dart';
+import 'package:tis/views/privacy.dart';
 
-class SettingScreen extends  StatefulWidget{
-  @override 
-     _SettingScreen createState() => _SettingScreen();  
+class SettingScreen extends StatefulWidget {
+  @override
+  _SettingScreen createState() => _SettingScreen();
 }
 
 class _SettingScreen extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-          appBar: AppBar(
-            backgroundColor:Color(0xffffffff),
-            leading: BackButton(
-              color: Colors.black
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffffffff),
+        leading: BackButton(color: Colors.black),
+        title: Text('Settings', style: TextStyle(color: Colors.black)),
+      ),
+      body: ListView(
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: [
+            ListTile(
+              title: Text('利用規約'),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Term()));
+              },
             ),
-            title: Text('Settings',style: TextStyle(
-              color: Colors.black
-            )),
-          ),
-      );
+            ListTile(
+              title: Text('プライバシーポリシー'),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Privacy()));
+              },
+            ),
+          ],
+        ).toList(),
+      ),
+    );
   }
-
 }
